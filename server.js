e0c7706c3d3d2cf77f9b951523bdfdff1b4db0da
duypnt23 <duypnt23@gmail.com>
2022-03-12 17:50:25 +0700

    update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const sql = require("mssql");
 const XlsxPopulate = require("xlsx-populate");
+const sqlConfig = require("./async-mssql");
 const rootRouter = require("./routers/root.router");
 const app = express();
 app.use(express.json());
@@ -8,6 +10,9 @@ const port = 5000;
 //get hello world
 app.get("/", (req, res) => res.send("Hello World!"));
 //get excel services
-app.use("/api/",rootRouter)
-
+app.use("/api/", rootRouter);
+sql.connect(sqlConfig, function (err) {
+  if (err) console.log(err);
+  else console.log("Connected");
+});
 app.listen(port, () => console.log(`Server started on port ${port}`));
