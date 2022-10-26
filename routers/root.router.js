e33702a283e63bd87f092c5e723fea28b0b83b83
asley duy <duypnt@gmail.com>
2022-10-26 15:26:20 +0700

    config

--- routers/root.router.js
+++ routers/root.router.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const rootRouter = express.Router();
 
-rootRouter.use("/excelServices", require("./excelServices.router"));
+
 module.exports = rootRouter;
--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const sql = require("mssql");
-const XlsxPopulate = require("xlsx-populate");
-const sqlConfig = require("./async-mssql");
 const rootRouter = require("./routers/root.router");
 const app = express();
 app.use(express.json());
@@ -11,8 +8,4 @@ const port = 5000;
 app.get("/", (req, res) => res.send("Hello World!"));
 //get excel services
 app.use("/api/", rootRouter);
-sql.connect(sqlConfig, function (err) {
-  if (err) console.log(err);
-  else console.log("Connected");
-});
 app.listen(port, () => console.log(`Server started on port ${port}`));
