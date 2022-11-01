50d53d91a64cc3620c4a7e8f0ac955e338d7088b
asley duy <duypnt@gmail.com>
2022-11-01 20:39:23 +0700

    update

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const rootRouter = require("./routers/root.router");
+const cors = require("cors");
+require('dotenv').config();
 const app = express();
+app.use(cors());
+
 app.use(express.json());
-const port = 5000;
+const port = process.env.SERVER_PORT;
 
 //get hello world
 app.get("/", (req, res) => res.send("Hello World!"));
 //get excel services
 app.use("/api/", rootRouter);
+
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
