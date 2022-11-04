8821b9b6c7252c84e076a5891029081997b3f4a1
Chí Hiển <89452066+ALX-Hien2k2@users.noreply.github.com>
2022-11-04 13:13:57 +0700

    api login, get userList

--- repositories/UserRepositories.js
+++ repositories/UserRepositories.js
@@ -1,17 +1,89 @@
 const getUserDetails = async (userId) => {
   const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(
+    try {
+      const users = [
         {
-          id: 1,
-          name: "John Doe",
+          id: 20127494,
+          name: "Truong Chi Hien",
         },
-        1000
-      );
-    });
+        {
+          id: 20127651,
+          name: "Do Minh Tri",
+        },
+        {
+          id: 20127016,
+          name: "Hai Baby",
+        },
+      ];
+      users.map((member) => {
+        if (member.id == userId) {
+          resolve(member);
+        }
+      });
+      reject("User does not exist");
+    } catch (err) {
+      reject(err);
+    }
   });
   return promise;
 };
+
+const loginAcc = async (userAccount) => {
+  const promise = new Promise((resolve, reject) => {
+    try {
+      const accounts = [
+        {
+          userName: "trchihien2002",
+          pwd: "12345"
+        },
+        {
+          userName: "dominhtri",
+          pwd: "zxcv"
+        },
+        {
+          userName: "haibabycute",
+          pwd: "xxxx"
+        }
+      ];
+
+      for (let i = 0; i < accounts.length; i++) {
+        if (accounts[i].userName == userAccount.userName && accounts[i].pwd == userAccount.pwd) {
+          resolve("Login successfully");
+        }
+      }
+      reject("Login failed");
+    } catch (err) {
+      reject(err);
+    }
+  });
+  return promise;
+};
+
+const getUserList = async () => {
+  const promise = new Promise((resolve, reject) => {
+    try {
+      const users = [
+        {
+          id: 20127494,
+          name: "Truong Chi Hien",
+        },
+        {
+          id: 20127651,
+          name: "Do Minh Tri",
+        },
+        {
+          id: 20127016,
+          name: "Hai Baby",
+        },
+      ];
+      resolve(users);
+    } catch (err) {
+      reject(err);
+    }
+  });
+  return promise;
+};
+
 module.exports = {
-  getUserDetails,
+  getUserDetails, loginAcc, getUserList
 };
--- routers/user.router.js
+++ routers/user.router.js
@@ -1,23 +1,51 @@
 const express = require("express");
-const { getUserDetails } = require("../repositories/UserRepositories");
+const { getUserDetails, loginAcc, getUserList } = require("../repositories/UserRepositories");
 const userRouter = express.Router();
 
 //  Test get request
 userRouter.get("/", (req, res) => {
-  res.send("Hello from userRouter");
+  res.send("Hello from userRouter get");
 });
+
 //  Test post request
 userRouter.post("/", (req, res) => {
   console.log("req.body", req.body);
-  res.send("Hello from userRouter");
+  res.send("Hello from userRouter post");
 });
 
-userRouter.get("/:uid", (req, res) => {
+// Get user info
+userRouter.get("/info/:uid", (req, res) => {
   const userId = req.params.uid;
   console.log("userId", userId);
+
   getUserDetails(userId)
     .then((user) => {
       res.send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+// Get users list
+userRouter.get("/list", (req, res) => {
+  getUserList()
+    .then((userList) => {
+      res.send(userList);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+// Login
+userRouter.post("/login", (req, res) => {
+  console.log("req.body", req.body);
+  const userAccount = req.body;
+
+  loginAcc(userAccount)
+    .then((status) => {
+      res.send(status);
     })
     .catch((err) => {
       res.status(400).send(err);
